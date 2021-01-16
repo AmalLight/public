a664865b2e79f87389200a5ef12f932f249b5a60
AmalLight <61798154+AmalLight@users.noreply.github.com>
2021-01-16 21:43:09 +0100

    Update forwarding.ps1

--- CyberSecurity Container Inject [Virus]/ContainerX/forwarding.ps1
+++ CyberSecurity Container Inject [Virus]/ContainerX/forwarding.ps1
@@ -3,31 +3,31 @@ param( $addordel , $ipsrc,$portsrc , $ipdest,$portdest )
 if ( ( $addordel.length -gt 0 ) -and
 
      ( ( $addordel        -eq 1 ) -and 
-	   ( $ipsrc.length    -gt 0 ) -and
-	   ( $portsrc.length  -gt 0 ) -and
-	   ( $ipdest.length   -gt 0 ) -and
-	   ( $portdest.length -gt 0 ) ) -or
+       ( $ipsrc.length    -gt 0 ) -and
+       ( $portsrc.length  -gt 0 ) -and
+       ( $ipdest.length   -gt 0 ) -and
+       ( $portdest.length -gt 0 ) ) -or
 		
-	 ( ( $addordel       -eq 2 ) -and
+     ( ( $addordel       -eq 2 ) -and
        ( $ipsrc.length   -gt 0 ) -and
-	   ( $portsrc.length -gt 0 ) ) )
+       ( $portsrc.length -gt 0 ) ) )
 {
-	if     ( $addordel -eq 1 )
-	{
-		netsh interface portproxy add v4tov4 listenaddress=$ipsrc listenport=$portsrc connectaddress=$ipdest connectport=$portdest
-		echo `netsh interface portproxy add v4tov4 listenaddress=$ipsrc listenport=$portsrc connectaddress=$ipdest connectport=$portdest`
-	}
-	elseif ( $addordel -eq 2 )
-	{
-		netsh interface portproxy delete v4tov4 listenaddress=$ipsrc listenport=$portsrc
-		echo `netsh interface portproxy delete v4tov4 listenaddress=$ipsrc listenport=$portsrc`
-	}
+    if     ( $addordel -eq 1 )
+    {
+        netsh interface portproxy add v4tov4 listenaddress=$ipsrc listenport=$portsrc connectaddress=$ipdest connectport=$portdest
+        echo `netsh interface portproxy add v4tov4 listenaddress=$ipsrc listenport=$portsrc connectaddress=$ipdest connectport=$portdest`
+    }
+    elseif ( $addordel -eq 2 )
+    {
+        netsh interface portproxy delete v4tov4 listenaddress=$ipsrc listenport=$portsrc
+        echo `netsh interface portproxy delete v4tov4 listenaddress=$ipsrc listenport=$portsrc`
+    }
 }
 else
 {
-	echo 'arg1: addordel, must be 1 or 2'
-	echo 'arg2: ipsrc    '
-	echo 'arg3: portsrc  '
-	echo 'arg4: ipdest   '
-	echo 'arg5: portdest '
+    echo 'arg1: addordel, must be 1 or 2'
+    echo 'arg2: ipsrc    '
+    echo 'arg3: portsrc  '
+    echo 'arg4: ipdest   '
+    echo 'arg5: portdest '
 }
